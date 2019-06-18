afe51a784ed723ccb491fa1c60d4a524ce7391e0
Oleksandr Iegorov <alex.yegorov@gmail.com>
2019-06-18 17:43:04 -0400

    Add stress-ng package to be installed

--- bootstrap_scripts/install_utilities.sh
+++ bootstrap_scripts/install_utilities.sh
@@ -2,4 +2,4 @@
 
 yum install -y epel-release
 rpm -Uvh http://yum.puppet.com/puppet6/puppet6-release-el-7.noarch.rpm
-yum install -y puppet-agent vim bind-utils net-tools bash-completion telnet tcpdump jq
+yum install -y puppet-agent vim bind-utils net-tools bash-completion telnet tcpdump jq stress-ng
